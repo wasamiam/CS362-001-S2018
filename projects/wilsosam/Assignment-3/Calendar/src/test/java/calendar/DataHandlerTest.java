2bf1c0ce5625f276f3101025f8bf152c9af038f1
wasamiam <wasamiam@gmail.com>
2018-05-09 23:10:29 -0700

    Tests

--- projects/wilsosam/Assignment-3/Calendar/src/test/java/calendar/DataHandlerTest.java
+++ projects/wilsosam/Assignment-3/Calendar/src/test/java/calendar/DataHandlerTest.java
@@ -18,15 +18,14 @@ public class DataHandlerTest{
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
-    //DataHandler dh1 = new DataHandler();
-      //assertTrue(dh1.fileName!=null);
+    DataHandler dh1 = new DataHandler("test");
     //assertTrue(dh1.save());
 
       //Appt ap1 = new Appt(0,0,0,"","","");
       //assertFalse(dh1.deleteAppt(ap1));
-    //Appt ap2 = new Appt(1,1,1,"","","");
-    //assertTrue(dh1.saveAppt(ap2));
-    //assertTrue(ap2.getXmlElement()!=null);
+    Appt ap2 = new Appt(1,1,1,"","","");
+    assertTrue(dh1.saveAppt(ap2));
+    assertTrue(ap2.getXmlElement()!=null);
 
       //assertTrue();
       //assertFalse(dh1.deleteAppt(ap2));
